ad01305a4c64eb830ccbd245be495a56f8436dd7
Th0re <stephane.perrez@gmail.com>
2017-05-15 09:56:01 +0200

    Started to separate the Humanoid in a new Class

---
+++ src/humanoid.cpp
@@ -0,0 +1,6 @@
+#include "humanoid.h"
+
+Humanoid::Humanoid()
+{
+
+}
---
+++ src/humanoid.h
@@ -0,0 +1,11 @@
+#ifndef HUMANOID_H
+#define HUMANOID_H
+
+
+class Humanoid
+{
+public:
+    Humanoid();
+};
+
+#endif // HUMANOID_H
