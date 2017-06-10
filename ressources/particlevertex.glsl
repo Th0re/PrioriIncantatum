f696a17cfeeb056c65e0d77df874efb9a9018c57
Th0re <stephane.perrez@gmail.com>
2017-06-10 02:45:05 +0200

    Particles as points kinda working

--- ressources/particlevertex.glsl
+++ ressources/particlevertex.glsl
@@ -4,20 +4,18 @@ uniform mat4 mvp;
 
 uniform int time;
 
-in vec3 position;
-in vec3 color;
+uniform vec3 position;
 in float angle;
 in float index;
 
-out vec3 fColor;
 out float ftime;
 
 //! [0]
 void main()
 {
     float t = (( ( time + int(4000*index) )%4001)/4000.);
-    float tx = t*10.;
-    float ty =   (2.*t- 4.*t*t)*10.;
+    float tx = t*100.;
+    float ty =   (2.*t- 4.*t*t)*100.;
     mat4 rot = mat4(cos(angle), 0., -sin(angle), 0.,
                     0.,         1., 0.,          0.,
                     sin(angle), 0., cos(angle),  0.,
@@ -31,6 +29,5 @@ void main()
     // Calculate vertex position in screen space
     gl_Position = mvp * rot * transl * vec4( position, 1.0 );
 
-    fColor = color;
     ftime = t;
 }
--- src/particulefountain.cpp
+++ src/particulefountain.cpp
@@ -4,26 +4,78 @@
 //{
 //}
 
+struct ParticleData
+{
+    float index;
+    float angle;
+};
+
 ParticuleFountain::ParticuleFountain(int nbParticles)
 {
-    particle = new Particle();
-    this->nbParticles = nbParticles;
-    for (int i = 0; i < nbParticles; i++) {
-        angles.append(3.14*random());
+    initializeOpenGLFunctions();
+
+    QImage image = QImage(QString(":/particle.bmp"), "BMP");
+    if (image.isNull()) {
+        throw 42;
+    } else {
+        texture = new QOpenGLTexture(image);
     }
+
+    this->nbParticles = nbParticles;
+    arrayBuf.create();
+
+    initGeometry();
+
 }
 
 ParticuleFountain::~ParticuleFountain()
 {
-    delete particle;
+    arrayBuf.destroy();
+    delete texture;
+}
+
+void ParticuleFountain::initGeometry() {
+
+    ParticleData particles[nbParticles];
+
+    for (int i = 0; i < nbParticles; i++) {
+        ParticleData particle;
+        particle.angle = 3.14*2*random()/((float)RAND_MAX);
+        particle.index = i/(float)nbParticles;
+        particles[i] =  particle;
+    }
+
+    arrayBuf.bind();
+    arrayBuf.allocate(particles, nbParticles*sizeof(ParticleData));
 }
 
 void ParticuleFountain::drawGeometry(QOpenGLShaderProgram *particleShader, int elapsedTime)
 {
-    for (int i = 0; i < nbParticles; i++) {
-        particleShader->setUniformValue("time", elapsedTime);
-        particleShader->setAttributeValue("index", i/(float)nbParticles);
-        particleShader->setAttributeValue("angle", angles[i]);
-        particle->drawGeometry(particleShader);
-    }
+
+
+    arrayBuf.bind();
+
+    // Offset for position
+    quintptr offset = 0;
+
+    // Tell OpenGL programmable pipeline how to locate vertex position data
+    int indexLocation = particleShader->attributeLocation("index");
+    particleShader->enableAttributeArray(indexLocation);
+    particleShader->setAttributeBuffer(indexLocation, GL_FLOAT, offset, 3, sizeof(ParticleData));
+
+    // Offset for texture coordinate
+    offset += sizeof(float);
+
+    // Tell OpenGL programmable pipeline how to locate vertex texture coordinate data
+    int angleLocation = particleShader->attributeLocation("angle");
+    particleShader->enableAttributeArray(angleLocation);
+    particleShader->setAttributeBuffer(angleLocation, GL_FLOAT, offset, 3, sizeof(ParticleData));
+
+
+    particleShader->setUniformValue("time", elapsedTime);
+
+    texture->bind();
+
+    glPointSize(5.);
+    glDrawArrays(GL_POINTS, 0, nbParticles);
 }
